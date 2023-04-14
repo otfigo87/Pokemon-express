4d7acef4ce9ed2462c5129550854f3f1a09a5839
Otmane Aatik <aaotmane@gmail.com>
2023-04-13 22:58:08 -0400

    I can view a list of all my pokemon in the browser

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
     res.send("Welcome to the Pokemon App!")
 })
 app.get("/pokemon", (req, res) => {
-    res.render('Index')
+    res.render('Index', {pokemons: pokemon})
 })
 
 app.listen(port, (req, res) => {
--- views/Index.jsx
+++ views/Index.jsx
@@ -5,12 +5,26 @@ const style = {
     color: '#400303',
     textAlign: 'center',
 }
+const list = {
+  listStyle: "none",
+  margin: "10px",
+  fontSize: "1.5em"
+}
 
-const Index = () => {
+const Index = ({pokemons}) => {
     return (
       <body style={style}>
         <div>
           <h1>Pokemon List</h1>
+          <div>
+            <ul>
+              {pokemons.map((pokemon, i) => (
+                <li key={i} style={list}>
+                  {pokemon.name.charAt(0).toUpperCase()+pokemon.name.slice(1)}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </body>
     );
