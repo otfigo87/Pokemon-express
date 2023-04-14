3d9abcc267ced367ef21970089bd0ab92579bb48
Otmane Aatik <aaotmane@gmail.com>
2023-04-13 22:38:55 -0400

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const pokemon = require('./models/pokemon');
 const app = express();
 const port = 3000;
+//views engine config
+app.set("view engine", "jsx");
+app.engine("jsx", require("jsx-view-engine").createEngine());
 
+//routes
 app.get("/", (req, res) => {
     res.send("Welcome to the Pokemon App!")
 })
 app.get("/pokemon", (req, res) => {
-    res.send(pokemon)
+    res.render('Index')
 })
 
 app.listen(port, (req, res) => {
---
+++ views/Index.jsx
@@ -0,0 +1,19 @@
+const React = require('react');
+
+const style = {
+    backgroundColor: '#c59696',
+    color: '#400303',
+    textAlign: 'center',
+}
+
+const Index = () => {
+    return (
+      <body style={style}>
+        <div>
+          <h1>Pokemon List</h1>
+        </div>
+      </body>
+    );
+}
+
+module.exports = Index;
